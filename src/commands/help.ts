cc4baa8ba6dc4ac17569fa27ace47786528c40e5
vieiraricardo <hi@ricvieira.com>
2021-08-01 21:24:07 -0300

    version 1.1.2

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,17 +1,15 @@
 import { GluegunToolbox } from 'gluegun'
 
-import { version } from '../../package.json'
-
 export default {
   name: 'help',
   dashed: true,
   alias: ['h'],
   description: 'Displays auto-matic CLI help',
   run: async function(toolbox: GluegunToolbox) {
-    const { print } = toolbox
+    const { print, meta } = toolbox
 
     print.info(`
-      auto-matic version ${version}\n
+      auto-matic version ${meta.version()}\n
       usage: auto-matic 0x0000000000000000000000000000000000000000\n
       commands:\n
       --help, -h: ${this.description}\n
